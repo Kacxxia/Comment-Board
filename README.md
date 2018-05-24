41a7e1df9be27b9cade15a45660767389af26777
kacxxia <kacxxia@gmail.com>
2018-05-24 23:25:22 +0800

    generate prod

--- README.md
+++
@@ -1,2 +0,0 @@
-## Comment-board 留言板
-*一个简单的React留言板，学习pouchDB以及测试、部署*
--- src/components/NoCommentYet/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from "./index.css"
-
-export const NoCommentYet = () => {
-    return <div className={styles.no_comment_yet}><em>暂时还没有人留言</em></div>
-}
-
-export default NoCommentYet
